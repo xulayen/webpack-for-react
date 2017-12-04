6dc7d36ca742b9bccdf6c45fd619945cdcdf0a60
xulayen <xulayen@163.com>
2017-12-04 13:39:18 +0800

    modify config data

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 
 
     let config={
-        url:'http://temp2.zhsh.co/OutAcCodeService.asmx?wsdl',
+        url:'',
         token:'M1q8G3yEF0',
         userID:'092KSO3LD82JS99S03KJAKQ891KD9',
         userPwd:'5A2E8D2G2Q2CDOIIKK9SLDKAJIS3S',
